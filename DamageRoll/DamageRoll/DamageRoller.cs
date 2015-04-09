f0e806dd609dc31739c05725f18021f9bbfe51a7
djolker <dj.olker@gmail.com>
2015-04-09 10:52:00 -0500

    Posting Current Version

---
+++ DamageRoll/DamageRoll/DamageRoller.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageRoll
+{
+    class DamageRoller
+    {  
+        public static float calcDamage(hero h, attackRoll a) 
+        {
+            //Physical Damage
+            float returnedPhys = calcPhysResist(h.armor, a.physicalDamage);
+            float finalPhys = a.physicalDamage - (a.physicalDamage * returnedPhys);
+            
+            //Fire Damage
+            float returnedFire = calcFireResist(h.fireResist, a.fireDamage);
+            float finalFire = a.fireDamage - (a.fireDamage * returnedFire);
+
+            //Cold Damage
+            float returnedCold = calcColdResist(h.coldResist, a.coldDamage);
+            float finalCold = a.coldDamage - (a.coldDamage * returnedCold);
+
+            //Light Damage
+            float returnedLight = calclightResist(h.lightResist, a.lightDamage);
+            float finalLight = a.lightDamage - (a.lightDamage * returnedLight);
+
+            //Calc Total
+            float total = finalPhys + finalFire + finalCold + finalLight;
+
+            if (a.isCrit)
+                Console.WriteLine("**CRITICAL HIT**");
+
+            Console.WriteLine("Physical Damage: {0}", finalPhys);
+            Console.WriteLine("Fire Damage: {0}", finalFire);
+            Console.WriteLine("Cold Damage: {0}", finalCold);
+            Console.WriteLine("Light Damage: {0}", finalLight);
+            Console.WriteLine("Total Damage: {0}", total);
+
+            return total;
+        }
+
+        public static float calcPhysResist(float armor, float damage)
+        {
+            float damageTaken = armor / (armor + (12 * damage));
+            return damageTaken;
+        }
+
+        public static float calcFireResist(float fireResist, float fireDamage)
+        {
+            float damageTaken = fireResist / (fireResist + (12 * fireDamage));
+            return damageTaken;
+        }
+
+        public static float calcColdResist(float coldResist, float coldDamage)
+        {
+            float damageTaken = coldResist / (coldResist + (12 * coldDamage));
+            return damageTaken;
+        }
+
+        public static float calclightResist(float lightResist, float lightDamage)
+        {
+            float damageTaken = lightResist / (lightResist + (12 * lightDamage));
+            return damageTaken;
+        }
+    }
+}
---
+++ DamageRoll/DamageRoll/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageRoll
+{
+    class hero
+    {
+        public float health = 100;
+        public float armor = 20;
+        public float fireResist = 15;
+        public float coldResist = 0;
+        public float lightResist = 90;
+        public float damage = 15;
+    }
+
+    public class attackRoll
+    {
+        public bool isCrit = false;
+
+        public float critStrikeChance = 19.9f;
+        public float critMult = 206;
+
+        public float physicalDamage;
+        public float fireDamage;
+        public float coldDamage;
+        public float lightDamage;
+
+        public attackRoll()
+        {
+            Random r = new Random();
+            float critRoll = float.Parse(r.NextDouble().ToString());
+
+            critRoll *= 100;
+
+            if (critRoll <= critStrikeChance)
+            {
+                isCrit = true;
+                
+                physicalDamage = r.Next(1, 10) * critMult / 100;
+                fireDamage = r.Next(1, 10) * critMult / 100;
+                coldDamage = r.Next(1, 10) * critMult / 100;
+                lightDamage = r.Next(1, 10) * critMult / 100;
+            }
+            else
+            {
+                physicalDamage = r.Next(1, 10);
+                fireDamage = r.Next(1, 10);
+                coldDamage = r.Next(1, 10);
+                lightDamage = r.Next(1, 10);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Run();
+                Console.ReadLine();
+            }
+        }
+
+        static public void Run()
+        {
+            hero h = new hero();
+            attackRoll a = new attackRoll();
+            DamageRoller.calcDamage(h, a);
+        }
+    }
+}
